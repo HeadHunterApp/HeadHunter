f89c1827eaae00b364062a7ba14f952b7973348c
bullky11 <pozsmarc112@gmail.com>
2024-03-25 05:57:35 +0100

    Regisztral form változtatás
    név,email,jelszó +magyarítás+legördülő nyelvmenü

--- HeadHunter_Frontend/hhreact/src/components/RegisztralForm.js
+++ HeadHunter_Frontend/hhreact/src/components/RegisztralForm.js
@@ -1,112 +1,140 @@
-import React, { useState } from 'react';
-import '../styles/Regisztral.css';
+import React, { useState } from "react";
+import "../styles/Regisztral.css";
 
 const Regisztral = () => {
-    const [formData, setFormData] = useState({
-        nem: '',
-        szul_ido: '',
-        telefonszam: '',
-        fax: '',
-        allampolgarsag: 'magyar',
-        jogositvany: false,
-        szoc_keszseg: ''
+  const [formData, setFormData] = useState({
+    nev: "",
+    email: "",
+    jelszo: "",
+    nem: "",
+    szul_ido: "",
+    telefonszam: "",
+    fax: "",
+    allampolgarsag: "magyar",
+    jogositvany: false,
+    szoc_keszseg: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
     });
+  };
 
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({
-            ...formData,
-            [name]: value
-        });
-    };
+  const handleCheckboxChange = (e) => {
+    const { name, checked } = e.target;
+    setFormData({
+      ...formData,
+      [name]: checked,
+    });
+  };
 
-    const handleCheckboxChange = (e) => {
-        const { name, checked } = e.target;
-        setFormData({
-            ...formData,
-            [name]: checked
-        });
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(formData);
-    };
-
-    return (
-        <div className="container">
-            <header className="header">
-                <h1>Regisztrácó </h1>
-            </header>
-            <form onSubmit={handleSubmit} className="registration-form">
-                <div className="form-group">
-                    <label>Gender:</label>
-                    <input
-                        type="text"
-                        name="nem"
-                        value={formData.nem}
-                        onChange={handleInputChange}
-                    />
-                </div>
-                <div className="form-group">
-                    <label>Date of Birth:</label>
-                    <input
-                        type="date"
-                        name="szul_ido"
-                        value={formData.szul_ido}
-                        onChange={handleInputChange}
-                    />
-                </div>
-                <div className="form-group">
-                    <label>Phone Number:</label>
-                    <input
-                        type="text"
-                        name="telefonszam"
-                        value={formData.telefonszam}
-                        onChange={handleInputChange}
-                    />
-                </div>
-                <div className="form-group">
-                    <label>Fax:</label>
-                    <input
-                        type="text"
-                        name="fax"
-                        value={formData.fax}
-                        onChange={handleInputChange}
-                    />
-                </div>
-                <div className="form-group">
-                    <label>Citizenship:</label>
-                    <input
-                        type="text"
-                        name="allampolgarsag"
-                        value={formData.allampolgarsag}
-                        onChange={handleInputChange}
-                    />
-                </div>
-                <div className="form-group">
-                    <label>Driver's License:</label>
-                    <input
-                        type="checkbox"
-                        name="jogositvany"
-                        checked={formData.jogositvany}
-                        onChange={handleCheckboxChange}
-                    />
-                </div>
-                <div className="form-group">
-                    <label>Language Proficiency:</label>
-                    <textarea
-                        name="szoc_keszseg"
-                        value={formData.szoc_keszseg}
-                        onChange={handleInputChange}
-                    ></textarea>
-                </div>
-                <div className="form-group">
-                    <button type="submit">Submit</button>
-                </div>
-            </form>
+  return (
+    <div className="container">
+      <header className="header">
+        <h1>Regisztrácó </h1>
+      </header>
+      <form onSubmit={handleSubmit} className="registration-form">
+        <div className="form-group">
+          <label>teljes név:</label>
+          <input
+            type="text"
+            name="név"
+            value={formData.nev}
+            onChange={handleInputChange}
+          />
         </div>
-    );
+        <div className="form-group">
+          <label>email cím:</label>
+          <input
+            type="text"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>jelszó:</label>
+          <input
+            type="password"
+            name="jelszo"
+            value={formData.jelszo}
+            onChange={handleInputChange}
+
+            
+          />
+        </div>
+        <div className="form-group">
+          <label>születési Dátum:</label>
+          <input
+            type="date"
+            name="szul_ido"
+            value={formData.szul_ido}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>telefon szám:</label>
+          <input
+            type="text"
+            name="telefonszam"
+            value={formData.telefonszam}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>fax:</label>
+          <input
+            type="text"
+            name="fax"
+            value={formData.fax}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>állampolgárság:</label>
+          <input
+            type="text"
+            name="allampolgarsag"
+            value={formData.allampolgarsag}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>jogosítvány:</label>
+          <input
+            type="checkbox"
+            name="jogositvany"
+            checked={formData.jogositvany}
+            onChange={handleCheckboxChange}
+          />
+        </div>
+        <div className="form-group">
+          <label>nyelvtudás:</label>
+          <select
+            name="nyelvtudas"
+            value={formData.nyelvtudas}
+            onChange={handleInputChange}
+          >
+            <option value="">-- Válasszon --</option>
+            <option value="kezdo">Kezdő</option>
+            <option value="kozep">Középhaladó</option>
+            <option value="halado">Haladó</option>
+          </select>
+        </div>
+        <div className="form-group">
+          <button type="submit">elküld</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Regisztral;
