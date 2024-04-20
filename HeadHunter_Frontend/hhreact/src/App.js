5981cf5f0e0b26552f25228ab85038939a0df889
oviola01 <o.viola01@gmail.com>
2024-04-20 19:30:06 +0200

    debug-frontend-auth

--- HeadHunter_Frontend/hhreact/src/App.js
+++ HeadHunter_Frontend/hhreact/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 //import Main from "./components/Main";
 import "./styles/App.css";
 //layoutok:
@@ -12,61 +12,63 @@ import Profilok from "./pages/Profilok";
 import Allaskereses from "./pages/AllasKereses";
 //a többi page-et még létre kell hozni
 
-
 export default function App() {
   return (
-    <Routes>
+      <Routes>
+        <Route path="/" element={<VendegLayout />}>
+          {/*belépés nélkül is elérhető */}
+          <Route index element={<Fooldal />} />
+          <Route path="jobs" element={<Allaskereses />} />
 
-      <Route path="/" element={<VendegLayout />}>
-        {/*belépés nélkül is elérhető */}
-        <Route index element={<Fooldal />} />
-        <Route path="jobs" element={<Allaskereses />} />
-        
-        {/* jövőbeli route-ok:
-        
-        <Route path="seeker-info" element={<AllaskerInfo />} />
-        <Route path="employer-info" element={<MunkaltatoInfo />} />
-        <Route path="contact" element={<Kapcsolat />} />
-        <Route path="unauthorized" element={<JogosulatlanFelh />} />
+          {/* jövőbeli route-ok:
+          
+          <Route path="seeker-info" element={<AllaskerInfo />} />
+          <Route path="employer-info" element={<MunkaltatoInfo />} />
+          <Route path="contact" element={<Kapcsolat />} />
+          <Route path="unauthorized" element={<JogosulatlanFelh />} />
 
-        */}
-      </Route>
+          */}
+        </Route>
 
-      <Route path="/" element={<AuthLayout jogosultFelh={[]} />}>
-        {/*minden belépett felhasználó */}
-        <Route index element={<Kezdolap />} />
-        <Route path="profile" element={<Profilok />} />
-      </Route>
+        <Route path="home" element={<AuthLayout jogosultFelh={[]} />}>
+          {/*minden belépett felhasználó */}
+          <Route index element={<Kezdolap />} />
+          <Route path="profile" element={<Profilok />} />
+        </Route>
 
-      <Route path="seeker" element={<AuthLayout jogosultFelh={['álláskereső']} />}>
-        {/*elvileg nincs olyan route, amit csak ő érne el */}
-      </Route>
+        <Route
+          path="seeker"
+          element={<AuthLayout jogosultFelh={["álláskereső"]} />}
+        >
+          {/*elvileg nincs olyan route, amit csak ő érne el */}
+        </Route>
 
-      <Route path="hunter" element={<AuthLayout jogosultFelh={['fejvadász']} />}>
-        {/* jövőbeli route-ok:
-        
-        <Route path="jobseekers" element={<Allaskeresok />} />
-        <Route path="queries" element={<Lekerdezesek />} />
+        <Route
+          path="hunter"
+          element={<AuthLayout jogosultFelh={["fejvadász"]} />}
+        >
+          {/* jövőbeli route-ok:
+          
+          <Route path="jobseekers" element={<Allaskeresok />} />
+          <Route path="queries" element={<Lekerdezesek />} />
 
-        */}
-      </Route>
+          */}
+        </Route>
 
-      <Route path="admin" element={<AuthLayout jogosultFelh={['admin']} />}>
-        {/* jövőbeli route-ok:
-        
-        <Route path="jobseekers" element={<Allaskeresok />} />
-        <Route path="headhunters" element={<Fejvadaszok />} />
-        <Route path="queries" element={<Lekerdezesek />} />
-        <Route path="employers" element={<Munkaltatok />} />
-        <Route path="fields" element={<Teruletek />} />
-        <Route path="positions" element={<Poziciok />} />
-        <Route path="languages" element={<Nyelvtudas />} />
-        <Route path="skills" element={<Kepessegek />} />
+        <Route path="admin" element={<AuthLayout jogosultFelh={["admin"]} />}>
+          {/* jövőbeli route-ok:
+          
+          <Route path="jobseekers" element={<Allaskeresok />} />
+          <Route path="headhunters" element={<Fejvadaszok />} />
+          <Route path="queries" element={<Lekerdezesek />} />
+          <Route path="employers" element={<Munkaltatok />} />
+          <Route path="fields" element={<Teruletek />} />
+          <Route path="positions" element={<Poziciok />} />
+          <Route path="languages" element={<Nyelvtudas />} />
+          <Route path="skills" element={<Kepessegek />} />
 
-        */}
-        
-      </Route>
-
-    </Routes>
+          */}
+        </Route>
+      </Routes>
   );
 }
